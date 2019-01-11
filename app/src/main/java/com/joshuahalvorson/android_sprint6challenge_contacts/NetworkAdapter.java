f2d47110b942b4c4755d8cbba243c97e4326f587
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-11 09:01:01 -0800

    Added Network adapter.

---
+++ app/src/main/java/com/joshuahalvorson/android_sprint6challenge_contacts/NetworkAdapter.java
@@ -0,0 +1,69 @@
+package com.joshuahalvorson.android_sprint6challenge_contacts;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetworkAdapter {
+    public interface NetworkCallback {
+        void returnResult(Boolean success, String result);
+    }
+
+    public static void httpGetRequest(final String urlString, final NetworkCallback callback) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = "";
+                boolean success = false;
+                HttpURLConnection connection = null;
+                InputStream stream = null;
+                try {
+                    URL url = new URL(urlString);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+                    int responseCode = connection.getResponseCode();
+                    if(responseCode == HttpURLConnection.HTTP_OK) {
+                        stream = connection.getInputStream();
+                        if(stream != null) {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                            StringBuilder builder = new StringBuilder();
+                            String line = reader.readLine();
+                            while(line != null){
+                                builder.append(line);
+                                line = reader.readLine();
+                            }
+                            result = builder.toString();
+                            success = true;
+                        }
+                    } else {
+                        result = String.valueOf(responseCode);
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(connection != null) {
+                        connection.disconnect();
+                    }
+
+                    if(stream != null) {
+                        try {
+                            stream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    callback.returnResult(success, result);
+                }
+                //return result;
+
+            }
+        }).start();
+
+    }
+}
