8d64de020707a8ce07094119e55c6f3d5cbe173f
Patrick Martin <patrickmartin88@gmail.com>
2019-01-11 09:38:51 -0800

    Contact class complete with all get functions and JSONObject parsing.

--- app/src/main/java/com/example/patrickjmartin/android_sprint6challenge_contacts/Contact.java
+++ app/src/main/java/com/example/patrickjmartin/android_sprint6challenge_contacts/Contact.java
@@ -9,6 +9,8 @@ public class Contact {
 
     public Contact (JSONObject json) {
 
+        JSONObject pictures = null;
+
         try {
             JSONObject name = json.getJSONObject("name");
             String fullName = "";
@@ -16,11 +18,64 @@ public class Contact {
             titleName = name.getString("title") + ", ";
             firstName = name.getString("first") + ", ";
             lastName = name.getString("last") + ", ";
-            
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        try {
+            this.email = json.getString("email");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.phoneNumber = json.getString("phone");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            pictures = json.getJSONObject("picture");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.pictureLarge = pictures.getString("large");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.pictureThumb = pictures.getString("thumbnail");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
+    public String getTitleName() {
+        return titleName;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPictureLarge() {
+        return pictureLarge;
+    }
+
+    public String getPictureThumb() {
+        return pictureThumb;
+    }
 }
